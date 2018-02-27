c52461a7073046c8cd61cc5eb77cb0ade09da120
franzecs@gmail.com <franzecs@gmail.com>
2018-02-27 08:53:30 -0300

    three

--- src/main/java/com/ikytus/ak/controler/ErrorPagesController.java
+++ src/main/java/com/ikytus/ak/controler/ErrorPagesController.java
@@ -7,17 +7,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 public class ErrorPagesController {
 
-	@RequestMapping("/404")
+	@RequestMapping("404")
 	public String notFound() {
 		return "/error/404";
 	}
 	
-	@RequestMapping("/403")
+	@RequestMapping("403")
 	public String forbidden() {
 		return "/error/403";
 	}
 	
-	@RequestMapping("/500")
+	@RequestMapping("500")
 	public String internalServerError() {
 		return "/error/500";
 	}
