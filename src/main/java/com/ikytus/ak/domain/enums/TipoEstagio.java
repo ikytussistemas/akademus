9ca907cc71d4b0e3b82a408e5b5e4e83b57c39fd
franzecs@gmail.com <franzecs@gmail.com>
2018-08-04 10:42:49 -0300

    tipo estágio semestre 2018.2

--- src/main/java/com/ikytus/ak/domain/enums/TipoEstagio.java
+++ src/main/java/com/ikytus/ak/domain/enums/TipoEstagio.java
@@ -7,7 +7,7 @@ public enum TipoEstagio {
 	REALPENAL(3,"Real Penal"),
 	CURRICULAR(4,"Curricular"),
 	NAOCURRICULAR(5,"Não Curricular"),
-	SELECAO22JEC(6, "Pré-seleção 22 JEC");
+	SELECAO22JEC(6, "Pré-seleção 11º JEC");
 		
 	private int cod;
 	private String descricao;
